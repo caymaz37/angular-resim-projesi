207c32826cadbdd88a3f81b558d5de968a14f03b
caymaz37 <ycaymaz@yandex.com>
2022-03-27 14:03:50 +0000

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  currentPage = 0;
   images = [
     {
       title: 'Deniz Kum Güneş',
